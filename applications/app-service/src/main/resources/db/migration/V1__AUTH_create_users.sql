452281d6fa2892ea91802c4878f48936245692da
sebastiangiron <sebastian.giron@correounivalle.edu.co>
2025-09-13 15:25:17 -0500

    update sql migrations

--- applications/app-service/src/main/resources/db/migration/V1__AUTH_create_users.sql
+++ applications/app-service/src/main/resources/db/migration/V1__AUTH_create_users.sql
@@ -47,7 +47,11 @@ INSERT INTO public."users" (
   (2, 'Customer', 'Demo', 'customer@example.com', '00000002', '1234567891', '1991-02-02', 100.00, 2, 'Street 2', '$2a$10$gH1k1FcCbXkEKXe4eB31k.EiB0BjMQEwKbZM70ztUhq48HniZQQ6S'),
   (3, 'Analyst', 'Data', 'analyst@example.com', '00000003', '1234567892', '1992-03-03', 4000.00, 3, 'Street 3', '$2a$10$gH1k1FcCbXkEKXe4eB31k.EiB0BjMQEwKbZM70ztUhq48HniZQQ6S'),
   (4, 'Manager', 'Boss', 'manager@example.com', '00000004', '1234567893', '1993-04-04', 6000.00, 4, 'Street 4', '$2a$10$gH1k1FcCbXkEKXe4eB31k.EiB0BjMQEwKbZM70ztUhq48HniZQQ6S'),
-  (5, 'Support', 'Help', 'support@example.com', '00000005', '1234567894', '1994-05-05', 200.00, 5, 'Street 5', '$2a$10$gH1k1FcCbXkEKXe4eB31k.EiB0BjMQEwKbZM70ztUhq48HniZQQ6S')
+  (5, 'Support', 'Help', 'support@example.com', '00000005', '1234567894', '1994-05-05', 200.00, 5, 'Street 5', '$2a$10$gH1k1FcCbXkEKXe4eB31k.EiB0BjMQEwKbZM70ztUhq48HniZQQ6S'),
+  (6, 'Admin', 'System', 'giron.arcila.sebastian97@gmail.com', '00000006', '1234567890', '1990-01-01', 5000.00, 1, 'HQ', '$2a$10$gH1k1FcCbXkEKXe4eB31k.EiB0BjMQEwKbZM70ztUhq48HniZQQ6S'),
+  (7, 'Customer', 'Demo', 'sebastianplayplus@gmail.com', '00000007', '1234567891', '1991-02-02', 100.00, 2, 'Street 2', '$2a$10$gH1k1FcCbXkEKXe4eB31k.EiB0BjMQEwKbZM70ztUhq48HniZQQ6S')
+
+
 ON CONFLICT ("id") DO NOTHING;
 
 -- Sincronizar la secuencia del ID con el valor máximo actual en la tabla
